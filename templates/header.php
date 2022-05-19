7d90cd675b5ef0776f9e7a1727582b42875a5319
Carlos Gutierrez <cgutierrez@utp.edu.co>
2022-05-19 02:03:07 -0500

    add CDN for bootstrap icons

--- templates/header.php
+++ templates/header.php
@@ -8,7 +8,8 @@
 
     <!-- Bootstrap CSS CDN -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
+    
     <!-- DataTables CSS CDN -->
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.0/css/jquery.dataTables.css">    <link href="css/navbar-top-fixed.css" rel="stylesheet">
 
