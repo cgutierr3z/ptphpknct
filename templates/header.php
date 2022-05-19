ce703f45a0843107d7e910e12e2ea7d7360b787a
Carlos Gutierrez <cgutierrez@utp.edu.co>
2022-05-19 01:27:16 -0500

    update 2 for header.php, footer.php with CDNs for DataTables.js and add initialize script

--- templates/header.php
+++ templates/header.php
@@ -5,10 +5,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>CafeKONECTA</title>
+
     <!-- Bootstrap CSS CDN -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+
     <!-- DataTables CSS CDN -->
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.0/css/jquery.dataTables.css">    <link href="css/navbar-top-fixed.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/navbar-top-fixed.css" rel="stylesheet">
+  </head>
   </head>
   <body>
     <div class="shadow-lg p-3 mb-5 bg-white rounded">
