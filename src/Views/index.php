5dcf0a2fa7472a9618f4c86b609e1524487bf7e9
Carlos Gutierrez <cgutierrez@utp.edu.co>
2022-05-19 07:11:33 -0500

    add info from project to index.php

---
+++ src/Views/index.php
@@ -0,0 +1,15 @@
+<?php include "../../templates/header.php"; ?>
+  <!-- Aquí el código HTML de la aplicación -->
+
+  <main role="main" class="container mt-9">
+    <div class="jumbotron">
+      <h1>Bienvenido a CafeKONECTA</h1>
+      <p class="lead">Con este aplicativo podrás gestionar el inventario de los
+        productos con las acciones básicas CRUD y tambien podrás registrar las
+        ventas realizadas de acuerdo al stock de cada producto.</p>
+      <a class="btn btn-lg btn-outline-primary" href="#" role="button">Añadir Producto &raquo;</a>
+      <a class="btn btn-lg btn-outline-primary" href="#" role="button">Listar Productos &raquo;</a>
+    </div>
+  </main>
+
+<?php include "../../templates/footer.php"; ?>
