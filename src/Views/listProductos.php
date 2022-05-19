0041a9913c4278ad7dbf56129a2e0541beb665e1
Carlos Gutierrez <cgutierrez@utp.edu.co>
2022-05-19 04:36:22 -0500

    add modal to confirm delete product, edit icons for update and delete in listProductos.php, add script to initialize modal to delete producto in footer.php

--- src/Views/listProductos.php
+++ src/Views/listProductos.php
@@ -7,7 +7,11 @@ $listaProductos=$producto->listProductos();
 ?>
   <!-- Aquí el código HTML de la aplicación -->
   <main role="main" class="container" >
-    <h1>Lista de productos</h1>
+    <h1>Lista de productos
+      <a class="btn btn-primary" role="button" href="formAddProducto.php">
+      <i class="bi bi-plus-square-fill"> </i> Añadir Nuevo Producto
+    </a>
+  </h1>
 
     <table id="tableProductos" class="display" style="width:100%">
         <thead>
@@ -36,14 +40,14 @@ $listaProductos=$producto->listProductos();
               <td><?php echo $prod->getPeso() ?></td>
               <td><?php echo $prod->getStock() ?></td>
               <th>
-                <a class="btn btn-outline-primary" role="button" href="formUpdateProducto.php?uid=<?php echo $prod->getUid()?>&action=a">
-                  <i class="bi bi-pencil"></i>
+                <a class="btn btn-outline-primary" role="button" href="formUpdateProducto.php?uid=<?php echo $prod->getUid()?>">
+                  <i class="bi bi-pencil-square"></i>
                 </a>
               </th>
               <th>
-                  <a class="btn btn-outline-danger" role="button" href="../Controllers/crudProducto.php?uid=<?php echo $prod->getUid()?>&action=e">
-                    <i class="bi bi-trash-fill"></i>
-                  </a>
+                <button type="button" class="btn btn-outline-danger" data-toggle="modal" data-target="#exampleModal" data-whatever="<?php echo $prod->getUid()?>">
+                  <i class="bi bi-trash-fill"></i>
+                </button>
               </th>
             </tr>
           <?php
@@ -64,6 +68,35 @@ $listaProductos=$producto->listProductos();
           </tr>
         </tfoot>
     </table>
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">¿Continuar con la eliminación del producto?</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="mx-auto" width="100%">
+              <h3>¿Segur@ que desea continuar?</h3>
+            </div>
+            <form action="../Controllers/crudProducto.php" method="post">
+              <div class="form-group">
+                <input type="hidden" class="form-control" name="uid" id="recipient-name">
+                <input type='hidden' name='delete' value='delete'>
+                <div class="mx-auto" width="300px">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal"><i class="bi bi-arrow-bar-left"> </i>Cancelar</button>
+                  <button type="submit" class="btn btn-danger"><i class="bi bi-trash-fill"> </i>Eliminar Producto</button>
+                </div>
+              </div>
+            </form>
+
+
+        </div>
+      </div>
+    </div>
 
   </main>
 
