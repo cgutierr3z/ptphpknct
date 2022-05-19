05ed94e487137d8f0e5aa6d4cc029ae8ee981355
Carlos Gutierrez <cgutierrez@utp.edu.co>
2022-05-19 04:43:05 -0500

    crud actions working in crudProducto.php, formAddProdcto.php is working well with validations and db connection

---
+++ src/Controllers/crudProducto.php
@@ -0,0 +1,37 @@
+<?php
+//incluye la clase Producto
+require_once('../Models/modelProducto.php');
+
+$producto= new modelProducto();
+	// Se valida cual variable se recibe via _POST para ejecutar las acciones:
+	// $_POST['add'] crear
+	// $_POST['update'] actualizar
+	// $_POST['delete'] eliminar
+	if (isset($_POST['add'])) {
+    $producto->setNombre($_POST['nombre']);
+    $producto->setReferencia($_POST['referencia']);
+    $producto->setPrecio($_POST['precio']);
+    $producto->setCategoria($_POST['categoria']);
+    $producto->setPeso($_POST['peso']);
+    $producto->setStock($_POST['stock']);
+
+		$producto->addProducto($producto);
+		header('Location: ../Views/listProductos.php');
+
+  }elseif(isset($_POST['update'])){
+  		$producto->setUid($_POST['uid']);
+      $producto->setNombre($_POST['nombre']);
+      $producto->setReferencia($_POST['referencia']);
+      $producto->setPrecio($_POST['precio']);
+      $producto->setCategoria($_POST['categoria']);
+      $producto->setPeso($_POST['peso']);
+      $producto->setStock($_POST['stock']);
+
+  		$producto->updateProducto($producto);
+  		header('Location: ../Views/listProductos.php');
+
+	}elseif(isset($_POST['delete'])) {
+		$producto->deleteProducto($_POST['uid']);
+		header('Location: ../Views/listProductos.php');
+  }
+?>
