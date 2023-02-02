b18a902e94b80988736f753ca2d1c470c355af41
Carlos Gutierrez <cgutierrez@utp.edu.co>
2023-02-01 20:58:49 -0500

    fix addventa, add method listVentas

--- src/Models/modelVenta.php
+++ src/Models/modelVenta.php
@@ -8,8 +8,6 @@
 		private $cantidadVenta;
 		private $fechaVenta;
 
-    private $newstock;
-
 		function __construct(){}
 
     // Funciones para obtener e inicialiar datos de modelVenta
@@ -26,7 +24,7 @@
 		}
 
     public function setUidProducto($uidProducto){
-		  $this->$uidProducto = $uidProducto;
+		  $this->uidProducto = $uidProducto;
 		}
 
 		public function getCantidadVenta(){
@@ -45,17 +43,48 @@
       $this->fechaVenta = $fechaVenta;
 		}
 
-    public function addVentaProducto($producto,$venta){
-      $producto = new modelProducto();
-      $prod=$producto->getProducto($producto->getUid());
+    public function addVentaProducto($venta){
       $db=databaseConnection::connect();
+
+      $producto = new modelProducto();
+      $prod = $producto->getProducto($venta->getUidProducto());
+
       $insert=$db->prepare('INSERT INTO venta_producto (uid_producto,cantidad_venta) values (:uidProducto,:cantidadVenta)');
-      $insert->bindValue('uidProducto',$producto->getUid(), PDO::PARAM_STR);
-      $insert->bindValue('cantidadVenta',$venta, PDO::PARAM_STR);
-      $newstock = $producto->getStock() - $venta;
+      $insert->bindValue('uidProducto',$venta->getUidProducto(), PDO::PARAM_STR);
+      $insert->bindValue('cantidadVenta',$venta->getCantidadVenta(), PDO::PARAM_STR);
+      $newstock = $prod->getStock() - $venta->getCantidadVenta();
+      if($newstock < 0){
+        $_SESSION['error'] = true;
+        $_SESSION['message'] = "NO HAY STOCK DISPONIBLE";
+        return $_SESSION; 
+      }
       $prod->setStock($newstock);
       $prod->updateProductoStock($prod);
       $insert->execute();
+      $_SESSION['error'] = false;
+      $_SESSION['message'] = "LA VENTA SE HA REGISTRADO";
+      $db=databaseConnection::close();
     }
+
+    // Funcion para listar todos las ventas
+    public function listVentas()
+    {
+      $db = databaseConnection::connect();
+      $listVentas = [];
+      $select = $db->query('SELECT * FROM venta_producto');
+
+      foreach ($select->fetchAll() as $venta) {
+
+        $producto = new modelProducto();
+        $ref = $producto->getProducto($venta['uid_producto'])->getReferencia();
+
+        $ven['uid'] = $venta['uid'];
+        $ven['ref'] = $ref;
+        $ven['cant'] = $venta['cantidad_venta'];
+        $listVentas[] = $ven;
+      }
+      $db=databaseConnection::close();
+      return $listVentas;
+    }    
 
   }
