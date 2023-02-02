baecfbea440016729500f65a5f08d8752967463a
Carlos Gutierrez <cgutierrez@utp.edu.co>
2023-02-01 20:59:33 -0500

    fix action add

--- src/Controllers/crudVenta.php
+++ src/Controllers/crudVenta.php
@@ -1,16 +1,28 @@
 <?php
+
+
 require_once('../Models/modelProducto.php');
 require_once('../Models/modelVenta.php');
 
 $producto = new modelProducto();
 $venta = new modelVenta();
-	// Se valida cual variable se recibe via _POST para ejecutar las acciones:
-	// $_POST['add'] crear
-	if (isset($_POST['add'])) {
-    $venta->setUidProducto($_POST['uidProducto']);
-    $venta->setcantidadVenta($_POST['cantidadVenta']);
-		$venta->addVentaProducto($producto,$venta->getCantidadVenta());
-		header('Location: ../Views/listProductos.php');
+// Se valida cual variable se recibe via _POST para ejecutar las acciones:
+// $_POST['add'] crear
+if (isset($_POST['add'])) {
+	$uidp = $_POST['uidProducto'];
+	//print_r($_POST['uidProducto']);
+	//print_r($_POST['cantidadVenta']);
 
-  }
+	$venta->setUidProducto($uidp);
+	//print_r($venta);
+	$venta->setCantidadVenta($_POST['cantidadVenta']);
+
+	//print_r("\n".$uidp."\n");
+	//print_r($venta->getUidProducto());
+	//print_r($venta->getCantidadVenta());
+
+	//print_r($venta);
+	$venta->addVentaProducto($venta);
+	header('Location: ../Views/formVenta.php');
+}
 ?>
