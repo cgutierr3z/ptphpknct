36036d69a83e6e6c33d5cbddd0277d0347901255
Carlos Gutierrez <cgutierrez@utp.edu.co>
2023-02-01 20:59:19 -0500

    add method close

--- src/Controllers/db_connection.php
+++ src/Controllers/db_connection.php
@@ -1,16 +1,24 @@
 <?php
-	class  databaseConnection{
-		private static $dbconnection=NULL;
-		private function __construct (){}
+class  databaseConnection
+{
+	private static $dbconnection = NULL;
+	private function __construct()
+	{
+	}
 
-		public static function connect(){
-      try {
-        $pdo_options[PDO::ATTR_ERRMODE]=PDO::ERRMODE_EXCEPTION;
-  			self::$dbconnection= new PDO('mysql:host=localhost;dbname=cafekonecta_db','root','openyoursesamo',$pdo_options);
-  			return self::$dbconnection;
-      } catch(PDOException $error) {
-        echo $error->getMessage();
-      }
+	public static function connect()
+	{
+		try {
+			$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+			self::$dbconnection = new PDO('mysql:host=localhost;dbname=cafekonecta_db', 'root', '', $pdo_options);
+			return self::$dbconnection;
+		} catch (PDOException $error) {
+			echo $error->getMessage();
 		}
 	}
+
+	public static function close(){
+        self::$dbconnection = null;
+    }
+}
 ?>
