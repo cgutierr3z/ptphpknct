2d1efe8a164d066533e94a05bc44885aa07d821f
Carlos Gutierrez <cgutierrez@utp.edu.co>
2023-02-01 20:58:04 -0500

    add session messages and table for list ventas

--- src/Views/formVenta.php
+++ src/Views/formVenta.php
@@ -1,48 +1,105 @@
 <?php
 include "../../templates/header.php";
 require_once('../Models/modelProducto.php');
+require_once('../Models/modelVenta.php');
 
 $producto = new modelProducto();
-$listaProductos=$producto->listProductos();
+$listaProductos = $producto->listProductos();
+
+
+$venta = new modelVenta();
+$listaVentas = $venta->listVentas();
 ?>
-  <!-- Aquí el código HTML de la aplicación -->
+<!-- Aquí el código HTML de la aplicación -->
 
-  <main role="main" class="container" >
-    <h1>Registrar venta de producto</h1>
+<main role="main" class="container">
+
+  <?php
+  session_start();
+  if (isset($_SESSION['message'])) {
+  ?>
+    <div class="alert alert-<?php echo ($_SESSION['error']) ?  "danger" :  "success"; ?> alert-dismissible fade show" role="alert">
+      <b><?php echo $_SESSION['message']; ?></b>
+
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+  <?php
+    unset($_SESSION['message']);
+  }
+  ?>
+
+  <h1>Registrar venta de producto</h1>
 
 
-    <form action='#' method='post' class="needs-validation" novalidate>
-      <div class="form-row">
-        <div class="col-md-8 mb-3">
-          <label for="validationCustom01">UID Producto</label><br>
-          <select class="form-control selectpicker" data-live-search="true" class="form-control" id="validationCustom01" name="uidProducto" required>
-            <?php
-            foreach($listaProductos as $prod) {
-            ?>
-              <option data-tokens="<?php echo $prod->getUid() ?>"> UID: <?php echo $prod->getUid() ?> - NOMBRE: <?php echo $prod->getNombre() ?> - STOCK: <?php echo $prod->getStock() ?> </option>
-            <?php
-            }
-            ?>
-          </select>
-          <div class="invalid-feedback">
-            Campo obligaorio.
-          </div>
-        </div>
-        <div class="col-md-4 mb-3">
-          <label for="validationCustom03">Cantidad Venta</label>
-          <input type="number" class="form-control" id="validationCustom02" name="cantidadVenta" required>
-          <div class="invalid-feedback">
-            Campo obligaorio.
-          </div>
+  <form action='../Controllers/crudVenta.php' method='post' class="needs-validation" novalidate>
+    <div class="form-row">
+      <div class="col-md-8 mb-3">
+        <label for="validationCustom01">UID Producto</label><br>
+        <select class="form-control selectpicker" data-live-search="true" class="form-control" id="validationCustom01" name="uidProducto" required>
+          <?php
+          foreach ($listaProductos as $prod) {
+          ?>
+            <option value="<?php echo $prod->getUid() ?>" data-tokens="<?php echo $prod->getUid() ?>"> UID: <?php echo $prod->getUid() ?> - NOMBRE: <?php echo $prod->getNombre() ?> - STOCK: <?php echo $prod->getStock() ?> </option>
+          <?php
+          }
+          ?>
+        </select>
+        <div class="invalid-feedback">
+          Campo obligaorio.
         </div>
       </div>
+      <div class="col-md-4 mb-3">
+        <label for="validationCustom03">Cantidad Venta</label>
+        <input type="number" class="form-control" id="validationCustom02" name="cantidadVenta" required>
+        <div class="invalid-feedback">
+          Campo obligaorio.
+        </div>
+      </div>
+    </div>
 
-      <input type='hidden' name='add' value='add'>
-      <a class="btn btn-outline-secondary" role="button" href="listProductos.php"><i class="bi bi-arrow-bar-left"> </i>Volver</a>
-      <button class="btn btn-success" type="submit"><i class="bi bi-save-fill"> </i>Registrar Venta</button>
-    </form>
+    <input type='hidden' name='add' value='add'>
+    <a class="btn btn-outline-secondary" role="button" href="listProductos.php"><i class="bi bi-arrow-bar-left"> </i>Volver</a>
+    <button class="btn btn-success" type="submit"><i class="bi bi-save-fill"> </i>Registrar Venta</button>
+  </form>
 
-    <script>
+  <main role="main" class="container">
+    <h1>Listado de ventas </h1>
+
+    <table id="tableProductos" class="display" style="width:100%">
+      <thead>
+        <tr>
+          <th>UID</th>
+          <th>REFERENCIA</th>
+          <th>CANTIDAD</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        foreach ($listaVentas as $v) {
+        ?>
+          <tr>
+            <td><?php echo $v['uid'] ?></td>
+            <td><?php echo $v['ref'] ?></td>
+            <td><?php echo $v['cant'] ?></td>
+          </tr>
+        <?php
+        }
+        ?>
+      <tbody>
+      <tfoot>
+        <tr>
+          <th>UID</th>
+          <th>REFERENCIA</th>
+          <th>CANTIDAD</th>
+        </tr>
+      </tfoot>
+    </table>
+
+  </main>
+
+  <script>
     // Example starter JavaScript for disabling form submissions if there are invalid fields
     (function() {
       'use strict';
@@ -61,8 +118,8 @@ $listaProductos=$producto->listProductos();
         });
       }, false);
     })();
-    </script>
+  </script>
 
-  </main>
+</main>
 
 <?php include "../../templates/footer.php"; ?>
